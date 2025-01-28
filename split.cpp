2ee7d72e4f51e5b523168b138eb7c7dc3c072bac
Justin Shih <jrshih@usc.edu>
2025-01-28 22:51:04 +0000

    amdg

--- split.cpp
+++ split.cpp
@@ -18,6 +18,34 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  if (in == nullptr) {
+    odds = nullptr;
+    evens = nullptr;
+    return;
+  }
+
+  // Make a sub for the node so it doesnt get lost
+  Node* inSub = in->next;
+  if (in->value % 2 == 0) {
+    // Change the odd to point to the new in node
+    evens = in;
+    evens->next = nullptr;
+    in = nullptr;
+    // Recursive Line, using in sub and pointing to next node in the odd sequence
+    return split(inSub, odds, evens->next);
+  }
+
+  else {
+    odds = in;
+    odds->next = nullptr;
+    // Set the old in pointers to 0
+    in = nullptr;
+    return split(inSub, odds->next, evens);
+  }
+
+  in = nullptr;
+
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -9,9 +9,38 @@ You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
 
+#include <iostream>
 #include "split.h"
+
+void printList(Node* head) {
+  while (head != nullptr) {
+    std::cout << head->value << " -> ";
+    head = head->next;
+  }
+
+  std::cout << "nullptr" << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
+  Node* head = new Node{1, new Node{2, new Node{3, new Node{4, nullptr}}}};
 
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+
+  split(head, odds, evens);
+
+  std::cout << "Odds: ";
+  printList(odds);
+
+  std::cout << "Evens: ";
+  printList(evens);
+
+  delete odds->next->next;
+  delete odds ->next;
+  delete odds;
+  delete evens ->next;
+  delete evens;
+  
+  return 0;
 }
